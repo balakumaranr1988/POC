39fe9386f8cb78b5808291474661fdb23cab0687
balakumaranr1988 <69300920+balakumaranr1988@users.noreply.github.com>
2020-08-06 14:56:34 +0530

    Add files via upload

---
+++ CustomerUpdation/src/main/java/com/Customer/Updation/models/CustomerUpdateDetail.java
@@ -0,0 +1,56 @@
+package com.Customer.Updation.models;
+
+public class CustomerUpdateDetail {
+	
+	String user_name;
+	String first_name;
+	String email;
+	String address;
+	String pan;
+	String dob;
+	String password;
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPan() {
+		return pan;
+	}
+	public void setPan(String pan) {
+		this.pan = pan;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+}
